e6180e6814bb706c6f3a4d1cbb788b57a63f4ac5
JSyndicate <jevwilliamson@gmail.com>
2023-02-28 16:41:54 -0500

    Created Database

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
@@ -88,6 +88,12 @@ def load_user(id):
 # The functions below should be applicable to all Flask apps.
 ###
 
+def get_uploaded_images():
+    rootdir = os.getcwd()
+    for subdir, dirs, files in os.walk(rootdir + '/uploads'):
+        fileNames = [n for n in files]
+    return fileNames
+
 # Flash errors from the form if validation fails
 def flash_errors(form):
     for field, errors in form.errors.items():
@@ -96,6 +102,14 @@ def flash_errors(form):
                 getattr(form, field).label.text,
                 error
 ), 'danger')
+            
+@app.route('/uploads/<filenmae>')
+def get_image(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
+
+@app.route('/file')
+def files():
+    return render_template(files.html, images = get_uploaded_images())
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
